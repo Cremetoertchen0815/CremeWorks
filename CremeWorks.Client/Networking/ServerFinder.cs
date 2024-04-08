32f2334dd327e83d3b45d86a3236c9f9b7308f68
Cremetoertchen0815 <Mircosoft_heck@web.de>
2024-04-08 17:51:43 +0200

    Added splash screen and server scan to client

---
+++ CremeWorks.Client/Networking/ServerFinder.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace CremeWorks.Client.Networking;
+public class ServerFinder
+{
+    private UdpClient _listenClient;
+
+    public event Action<IPAddress>? ServerFound;
+
+    private const int LISTENING_PORT = 25565;
+    private const string LISTENING_IDENTIFIER = "CremeWorks Listener v1.0";
+
+    public ServerFinder()
+    {
+        _listenClient = new UdpClient();
+        _listenClient.Client.Bind(new IPEndPoint(IPAddress.Any, LISTENING_PORT));
+    }
+
+    public async Task ListenAsync(CancellationToken cancelSource)
+    {
+        while (!cancelSource.IsCancellationRequested)
+        {
+            var recvBuffer = await _listenClient.ReceiveAsync(cancelSource);
+            var str = Encoding.ASCII.GetString(recvBuffer.Buffer);
+            if (str != LISTENING_IDENTIFIER) continue;
+            ServerFound?.Invoke(recvBuffer.RemoteEndPoint.Address);
+        }
+    }
+}
---
+++ CremeWorks.Client/SplashScreen.cs
@@ -0,0 +1,73 @@
+﻿using CremeWorks.Client.Networking;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CremeWorks.Client;
+public partial class SplashScreen : Form
+{
+    private const int FIND_TIMEOUT_SEC = 10;
+
+    public SplashScreen() => InitializeComponent();
+
+    private async void SplashScreen_Shown(object sender, EventArgs e)
+    {
+        IPAddress[] clients = [];
+        while (true)
+        {
+            var timeout = false;
+            try
+            {
+                clients = await FindServer();
+            }
+            catch (OperationCanceledException)
+            {
+                timeout = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+                return;
+            }
+
+            //Check if servers can be found
+            if (timeout || clients.Length == 0)
+            {
+                var userAction = MessageBox.Show("No servers have been found! Do you want to retry?", "Server Scan Timeout", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Error);
+                switch (userAction)
+                {
+                    case DialogResult.Yes:
+                        break;
+                    default:
+                        Application.Exit();
+                        return;
+                }
+
+                continue;
+            }
+
+            var serverIp = clients.Length == 1 ? clients[0] : ServerSelection.Show(clients);
+            if (serverIp is null) break;
+        }
+    }
+
+    private async Task<IPAddress[]> FindServer()
+    {
+        var serverList = new ConcurrentBag<IPAddress>();
+        var finder = new ServerFinder();
+        finder.ServerFound += serverList.Add;
+
+        var cSource = new CancellationTokenSource(TimeSpan.FromSeconds(FIND_TIMEOUT_SEC));
+        await finder.ListenAsync(cSource.Token);
+        return serverList.ToArray();
+    }
+}
