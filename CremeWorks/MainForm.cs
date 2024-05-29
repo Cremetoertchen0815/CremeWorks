7b00a74e848239cbdc032dc700d7ec68f778fad6
Cremetoertchen0815 <Mircosoft_heck@web.de>
2024-05-29 23:49:31 +0200

    Fixed bug that auto patch does not work

--- CremeWorks/MainForm.cs
+++ CremeWorks/MainForm.cs
@@ -119,7 +119,7 @@ namespace CremeWorks
         {
             _c.MidiMatrix.ActiveSong = _s;
 
-            for (int i = 0; i < Concert.PATCH_DEVICE_COUNT; i++) if (_s.AutoPatchSlots[i].Enabled) _s.AutoPatchSlots[i].Patch?.ApplyPatch(_c.Devices[i + 2]);
+            for (int i = 0; i < Concert.PATCH_DEVICE_COUNT; i++) if (_s.AutoPatchSlots[i].Enabled) _s.AutoPatchSlots[i].Patch?.ApplyPatch(_c.Devices[MIDIMatrix.INSTR_DEVICE_OFFSET + 1]);
         }
 
         private void ExecuteAction(int nr, bool? enable)
