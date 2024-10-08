1d5c3320487abd5f9b88c004f2f39fe70b64d2ff
Cremetoertchen0815 <Mircosoft_heck@web.de>
2024-10-08 17:30:09 +0200

    Fixed bug in the hashing algorithm

--- CremeWorks.Backend/Utils/PasswordHash.cs
+++ CremeWorks.Backend/Utils/PasswordHash.cs
@@ -10,12 +10,13 @@ public class PasswordHash
         using var saltedPassword = new MemoryStream();
         saltedPassword.Write(Encoding.UTF8.GetBytes(password));
         saltedPassword.Write(salt);
+        saltedPassword.Position = 0;
 
         // Hash the concatenated password and salt
         // & Concatenate the salt and hashed password for storage
         byte[] hashedPasswordWithSalt = new byte[SHA256.HashSizeInBytes + salt.Length];
-        await SHA256.HashDataAsync(saltedPassword, hashedPasswordWithSalt);
-        salt.CopyTo(hashedPasswordWithSalt.AsMemory(0, SHA256.HashSizeInBytes));
+        await SHA256.HashDataAsync(saltedPassword, hashedPasswordWithSalt.AsMemory(0, SHA256.HashSizeInBytes));
+        salt.CopyTo(hashedPasswordWithSalt.AsMemory(SHA256.HashSizeInBytes));
 
         return Convert.ToBase64String(hashedPasswordWithSalt);
     }
