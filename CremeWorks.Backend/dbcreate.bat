6b12c7ed12c1f0d9202daea6eeaed6d3900081b1
Cremetoertchen0815 <Mircosoft_heck@web.de>
2025-03-09 14:07:21 +0100

    Update backend credentials for DB

--- CremeWorks.Backend/dbcreate.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-docker pull mariadb
-docker create --name db-webserver -e MYSQL_ROOT_PASSWORD=dLfU8nN45sP -e MYSQL_DATABASE=webserver -e MYSQL_USER=dbuser -e MYSQL_PASSWORD=yY6.!26X7PNC -p 3310:3306 mariadb
-pause
